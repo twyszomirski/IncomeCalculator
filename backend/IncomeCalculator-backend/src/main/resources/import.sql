550d898b6573c6ddfa2017005f22e7f45c3f9bc6
tomasz.wyszomirski <tomasz.wyszomirski@heinfricke.com>
2015-10-25 21:16:00 +0100

    Insert demo data.

--- backend/IncomeCalculator-backend/src/main/resources/import.sql
+++ backend/IncomeCalculator-backend/src/main/resources/import.sql
@@ -1,2 +1,3 @@
-insert into country(name, currency_code,country_code,tax_rate,additional_cost) values ('Poland','PLN','PL',0.19,200)
-insert into country(name, currency_code,country_code,tax_rate,additional_cost) values ('UK','GBP','GB',0.19,200)
+insert into country(name, currency_code,country_code,tax_rate,additional_cost) values ('Niemcy','EUR','DE',0.20,800)
+insert into country(name, currency_code,country_code,tax_rate,additional_cost) values ('Polska','PLN','PL',0.19,1200)
+insert into country(name, currency_code,country_code,tax_rate,additional_cost) values ('Wielka Brytania','GBP','GB',0.25,600)
